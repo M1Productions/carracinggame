ddd650a93b388e9ee4b506d545bfabc0a6260c2e
Dennis_gunther <57572233+dennisgunter@users.noreply.github.com>
2022-02-10 09:56:25 +0100

    First structure

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# carracinggame
-This is a 2D car racing game like the arcade games in the 80s but online.
+Maximilian Prietzel, Valentin Tietze
---
+++ src/main.java
@@ -0,0 +1,51 @@
+import processing.core.*;
+import processing.data.*;
+import processing.event.*;
+import processing.opengl.*;
+
+/*
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+*/
+
+public class main extends PApplet {
+
+  // main setup function
+  public void setup() {
+    frameRate(30);
+
+    surface.setTitle("Car Racing Game");
+    // surface.setIcon(loadImage("Image.png"));
+  }
+
+  // main draw function
+  public void draw() {
+    background(0);
+    rect(0, 0, 500, 500);
+  }
+
+  // This function will be executed by the main method to get the settings for the
+  // Sketch
+  public void settings() {
+    size(700, 700);
+  }
+
+  // this is the main function, which starts the Sketch
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "main" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
